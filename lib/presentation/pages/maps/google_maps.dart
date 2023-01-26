3fd52d5f4c1047fbcb39296877d12edea01393d4
tinodewa <dtino0812@gmail.com>
2023-01-27 00:58:15 +0700

    add current user location to map

--- lib/presentation/pages/maps/google_maps.dart
+++ lib/presentation/pages/maps/google_maps.dart
@@ -18,27 +18,18 @@ class _GoogleMapsState extends State<GoogleMaps> {
 
   final LatLng _center = const LatLng(45.521563, -122.677433);
 
-  static final CameraPosition _kGoogle = const CameraPosition(
+  CameraPosition _kGoogle = const CameraPosition(
     target: LatLng(20.42796133580664, 80.885749655962),
     zoom: 14.4746,
   );
 
-  final List<Marker> _markers = <Marker>[
-    Marker(
-      markerId: MarkerId('1'),
-      position: LatLng(20.42796133580664, 80.885749655962),
-      infoWindow: InfoWindow(
-        title: 'My Position',
-      ),
-    ),
-  ];
+  final List<Marker> _markers = <Marker>[];
 
   Future<geolocator.Position> getUserCurrentLocation() async {
     await geolocator.Geolocator.requestPermission()
         .then((value) {})
         .onError((error, stackTrace) async {
       await geolocator.Geolocator.requestPermission();
-      print("ERROR" + error.toString());
     });
     return await geolocator.Geolocator.getCurrentPosition();
   }
@@ -52,17 +43,35 @@ class _GoogleMapsState extends State<GoogleMaps> {
       appBar: AppBar(
         title: const Text('Profil'),
         elevation: 0.5,
-        // actions: [
-        //   IconButton(
-        //     icon: const Icon(Icons.account_balance),
-        //     onPressed: () {},
-        //   ),
-        // ],
       ),
       body: SafeArea(
         child: GoogleMap(
           onMapCreated: (GoogleMapController controller) {
             _contoller.complete(controller);
+
+            getUserCurrentLocation().then(
+              (value) async {
+                _markers.add(
+                  Marker(
+                    markerId: const MarkerId("1"),
+                    position: LatLng(value.latitude, value.longitude),
+                    infoWindow: const InfoWindow(
+                      title: 'My Current Location',
+                    ),
+                  ),
+                );
+                _kGoogle = new CameraPosition(
+                  target: LatLng(value.latitude, value.longitude),
+                  zoom: 14,
+                );
+
+                final GoogleMapController controller = await _contoller.future;
+                controller.animateCamera(
+                  CameraUpdate.newCameraPosition(_kGoogle),
+                );
+                setState(() {});
+              },
+            );
           },
           initialCameraPosition: _kGoogle,
           markers: Set<Marker>.of(_markers),
@@ -71,37 +80,37 @@ class _GoogleMapsState extends State<GoogleMaps> {
           compassEnabled: true,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          getUserCurrentLocation().then(
-            (value) async {
-              print(
-                  value.latitude.toString() + " " + value.longitude.toString());
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () async {
+      //     getUserCurrentLocation().then(
+      //       (value) async {
+      //         print(
+      //             value.latitude.toString() + " " + value.longitude.toString());
 
-              _markers.add(
-                Marker(
-                  markerId: MarkerId("2"),
-                  position: LatLng(value.latitude, value.longitude),
-                  infoWindow: InfoWindow(
-                    title: 'My Current Location',
-                  ),
-                ),
-              );
-              CameraPosition cameraPosition = new CameraPosition(
-                target: LatLng(value.latitude, value.longitude),
-                zoom: 14,
-              );
+      //         _markers.add(
+      //           Marker(
+      //             markerId: MarkerId("2"),
+      //             position: LatLng(value.latitude, value.longitude),
+      //             infoWindow: InfoWindow(
+      //               title: 'My Current Location',
+      //             ),
+      //           ),
+      //         );
+      //         CameraPosition cameraPosition = new CameraPosition(
+      //           target: LatLng(value.latitude, value.longitude),
+      //           zoom: 14,
+      //         );
 
-              final GoogleMapController controller = await _contoller.future;
-              controller.animateCamera(
-                CameraUpdate.newCameraPosition(cameraPosition),
-              );
-              setState(() {});
-            },
-          );
-        },
-        child: Icon(Icons.local_activity),
-      ),
+      //         final GoogleMapController controller = await _contoller.future;
+      //         controller.animateCamera(
+      //           CameraUpdate.newCameraPosition(cameraPosition),
+      //         );
+      //         setState(() {});
+      //       },
+      //     );
+      //   },
+      //   child: Icon(Icons.local_activity),
+      // ),
     );
   }
 }
